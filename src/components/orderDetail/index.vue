3ab720e0360f1ad2012f8edc402fe500201d52b9
xionghq <18614084016@163.com>
2019-10-11 14:16:34 +0800

    订单详情提交代码

--- src/components/orderDetail/index.vue
+++ src/components/orderDetail/index.vue
@@ -46,6 +46,104 @@
                 </div>
             </div>
         </div>
+        <div class="work-info">
+            <div class="item1 row flex-item flex-justify-start">
+              <div class="item1-icon">
+
+              </div>
+              <div class="item1-title">
+                  工作履历
+              </div>
+            </div>
+            <div class="info">
+                从业经验丰富，照顾过早产儿。有丰富的产后调理经验， 可协助孕妈的身材尽快恢复。 性格乐观，爱干净。 最多500个汉字
+            </div>
+        </div>
+        <div class="home-info">
+            <div class="item1 row flex-item flex-justify-start">
+                <div class="item1-icon">
+
+                </div>
+                <div class="item1-title">
+                    家庭情况
+                </div>
+            </div>
+            <div class="info">
+                爱人工作情况，子女情况，最多展示两行
+            </div>
+        </div>
+        <div class="certificate-info">
+            <div class="item1 row flex-item flex-justify-start">
+                <div class="item1-icon">
+
+                </div>
+                <div class="item1-title">
+                    证书信息
+                </div>
+            </div>
+            <div class="info row flex-item flex-justify-start wrap">
+                <div class="item col flex-item flex-justify">
+                    <div class="item-icon">
+
+                    </div>
+                    <div class="item-text">
+                        按摩技师证
+                    </div>
+                </div>
+                <div class="item col flex-item flex-justify">
+                    <div class="item-icon">
+
+                    </div>
+                    <div class="item-text">
+                        按摩技师证
+                    </div>
+                </div>
+                <div class="item col flex-item flex-justify">
+                    <div class="item-icon">
+
+                    </div>
+                    <div class="item-text">
+                        按摩技师证
+                    </div>
+                </div>
+                <div class="item col flex-item flex-justify">
+                    <div class="item-icon">
+
+                    </div>
+                    <div class="item-text">
+                        按摩技师证
+                    </div>
+                </div>
+                <div class="item col flex-item flex-justify">
+                    <div class="item-icon">
+
+                    </div>
+                    <div class="item-text">
+                        按摩技师证
+                    </div>
+                </div>
+                <div class="item col flex-item flex-justify">
+                    <div class="item-icon">
+
+                    </div>
+                    <div class="item-text">
+                        按摩技师证
+                    </div>
+                </div>
+                <div class="item col flex-item flex-justify">
+                    <div class="item-icon">
+
+                    </div>
+                    <div class="item-text">
+                        按摩技师证
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="submit-btn">
+            立即预约
+        </div>
     </div>
 </template>
 
@@ -72,7 +170,6 @@
 <style scoped lang="scss">
     .container{
         width: 100%;
-        overflow: hidden;
     }
     .order-item{
         background: $white;
@@ -147,5 +244,146 @@
                 color:#3B445C;
             }
         }
+    }
+    .work-info{
+        margin-top: 10px;
+        min-height: 153px;
+        width: 100%;
+        overflow: hidden;
+        background: #FFFFFF;
+        .item1{
+            margin-top: 16px;
+            height: 20px;
+            width: 100%;
+            .item1-icon{
+                margin-left: 16px;
+                margin-right: 10px;
+                width: 14px;
+                height: 16px;
+                background: #999999;
+            }
+            .item1-title{
+                line-height: 20px;
+                font-size:16px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                color:rgba(0,2,18,1);
+            }
+        }
+        .info{
+            margin-left: 16px;
+            margin-top: 16px;
+            width: 343px;
+            height: 80px;
+            font-size:13px;
+            font-family:PingFangSC-Regular,PingFangSC;
+            font-weight:400;
+            color:rgba(59,68,92,1);
+            line-height:20px;
+        }
+    }
+
+    .home-info{
+        margin-top: 10px;
+        min-height: 92px;
+        width: 100%;
+        overflow: hidden;
+        background: #FFFFFF;
+        .item1{
+            margin-top: 16px;
+            height: 20px;
+            width: 100%;
+            .item1-icon{
+                margin-left: 16px;
+                margin-right: 10px;
+                width: 18px;
+                height: 18px;
+                background: #999999;
+            }
+            .item1-title{
+                line-height: 20px;
+                font-size:16px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                color:rgba(0,2,18,1);
+            }
+        }
+        .info{
+            margin-left: 16px;
+            margin-top: 16px;
+            width: 343px;
+            font-size:13px;
+            font-family:PingFangSC-Regular,PingFangSC;
+            font-weight:400;
+            color:rgba(59,68,92,1);
+            line-height:20px;
+        }
+    }
+
+
+    .certificate-info{
+        margin-top: 10px;
+        min-height: 310px;
+        width: 100%;
+        overflow: hidden;
+        background: #FFFFFF;
+        .item1{
+            margin-top: 16px;
+            height: 20px;
+            width: 100%;
+            .item1-icon{
+                margin-left: 16px;
+                margin-right: 10px;
+                width: 18px;
+                height: 18px;
+                background: #999999;
+            }
+            .item1-title{
+                line-height: 20px;
+                font-size:16px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                color:rgba(0,2,18,1);
+            }
+        }
+        .info{
+           margin-top: 30px;
+           width: 100%;
+            .item{
+                margin-bottom: 10px;
+                margin-left: 26px;
+                width: 60px;
+                height: 60px;
+                .item-icon{
+                   width: 22px;
+                   height: 18px;
+                    background: #666666;
+                    margin-bottom: 9px;
+                }
+                .item-text{
+                    font-size:12px;
+                    font-family:PingFangSC-Regular,PingFangSC;
+                    font-weight:400;
+                    color:rgba(59,68,92,1);
+                }
+
+            }
+        }
+    }
+
+    .submit-btn{
+        position: fixed;
+        bottom: 15px;
+        left: 40px;
+        width:296px;
+        height:42px;
+        line-height: 42px;
+        text-align: center;
+        background:linear-gradient(328deg,rgba(201,141,253,1) 0%,rgba(250,119,119,1) 100%);
+        border-radius:21px;
+        font-size:16px;
+        font-family:PingFangSC-Regular,PingFangSC;
+        font-weight:400;
+        color:rgba(255,255,255,1);
     }
 </style>
