5b81f3509be6423dfcf9db02c61dfa8dc2ea6023
xionghq <18614084016@163.com>
2019-10-11 17:47:37 +0800

    提交代码

---
+++ src/common/utils/awaitWrap.js
@@ -0,0 +1,5 @@
+export default (promise)=>{
+  return promise
+    .then(data=>[null,data])
+    .catch(err=>[err,null])
+}
---
+++ src/common/utils/constants.js
@@ -0,0 +1,1 @@
+export const REPAY_TYPE=[{name:'等本等息',id:1},{name:'等额本息',id:2},{name:'等额本金',id:3}]; //还款方式
