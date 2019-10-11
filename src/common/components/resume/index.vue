fbb72437029979b5033d149d9b3d87d2e098d984
xionghq <18614084016@163.com>
2019-10-11 10:32:56 +0800

    预约面试页面提交

---
+++ src/common/components/resume/index.vue
@@ -0,0 +1,194 @@
+<template>
+    <div class="resume-header">
+        <div class="bg"></div>
+        <div class="card-info row flex-item flex-justify-start">
+            <div class="header-img">
+                <img src="" alt="">
+            </div>
+            <div class="info col flex-item flex-justify">
+               <div class="item1 row flex-item flex-justify-between">
+                   <div class="name">
+                       张阿姨
+                   </div>
+                   <div class="money">
+                       ￥ 7000/月
+                   </div>
+               </div>
+               <div class="item2 row flex-item flex-justify-start">
+                  <div class="item2-icon">
+                      <img src="" alt="">
+                  </div>
+                   <div class="title">
+                       2018十佳育婴师
+                   </div>
+               </div>
+               <div class="item3 row flex-item flex-justify-start">
+                   <div class="item-3 col flex-item-start flex-justify">
+                       <div class="item-3-title">
+                           54岁
+                       </div>
+                       <div class="item-3-title2">
+                           年龄
+                       </div>
+
+                   </div>
+                   <div class="item-3 col flex-item-start flex-justify">
+                       <div class="item-3-title">
+                           2年
+                       </div>
+                       <div class="item-3-title2">
+                           经验
+                       </div>
+                   </div>
+                   <div class="item-3 col flex-item-start flex-justify">
+                       <div class="item-3-title">
+                           湖南
+                       </div>
+                       <div class="item-3-title2">
+                           籍贯
+                       </div>
+                   </div>
+                   <div class="item-3 col flex-item-start flex-justify">
+                       <div class="item-3-title">
+                           高中
+                       </div>
+                       <div class="item-3-title2">
+                           学历
+                       </div>
+                   </div>
+               </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+    //简历介绍 头部
+  export default {
+    name: 'resume',
+    data () {
+      return {}
+    },
+//组件
+    components: {},
+//初始化数据
+    created () {
+
+    },
+//一些自定义方法
+    methods: {}
+  }
+</script>
+
+<style scoped lang="scss">
+.resume-header{
+    width: 100%;
+    height: 190px;
+    background: $white;
+    position: relative;
+    .bg{
+        position: absolute;
+        top: 0;
+        left: 0;
+        width:100%;
+        height: 132px;
+        background:linear-gradient(338deg,rgba(201,141,253,1) 0%,rgba(250,119,119,1) 100%);
+    }
+    .card-info{
+        position: absolute;
+        top: 16px;
+        left: 13px;
+        width: 350px;
+        height: 150px;
+        border-radius:6px;
+        background: $white;
+        box-shadow:0 5px 10px -5px #999;
+        .header-img{
+            margin-left: 16px;
+            width: 114px;
+            height: 114px;
+            background: #999999;
+            border-radius: 4px;
+            overflow: hidden;
+            img{
+                display: block;
+                width: 100%;
+                height: 100%;
+            }
+
+        }
+        .info{
+           margin-left: 16px;
+           width: 188px;
+           height: 100%;
+           overflow: hidden;
+            .item1{
+                width: 100%;
+                height: 30px;
+                line-height: 30px;
+                .name{
+                    font-size:22px;
+                    font-family:PingFangSC-Semibold,PingFangSC;
+                    font-weight:600;
+                    color:rgba(3,8,24,1);
+                }
+                .money{
+                    font-family:PingFangSC-Medium,PingFangSC;
+                    font-weight:500;
+                    color:$pink;
+                }
+            }
+            .item2{
+                margin-top: 6px;
+                height: 16px;
+                width: 100%;
+                line-height: 16px;
+                .item2-icon{
+                    width: 12px;
+                    height: 12px;
+                    background: #999999;
+                    img{
+                        width: 100%;
+                        height: 100%;
+                    }
+                }
+                .title{
+                    margin-left: 6px;
+                    font-size:10px;
+                    font-family:PingFangSC-Regular,PingFangSC;
+                    font-weight:400;
+                    color:rgba(223,83,83,1);
+                }
+
+            }
+            .item3{
+                margin-top: 5px;
+                width: 100%;
+                height: 40px;
+                .item-3{
+                    margin-right: 8.5px;
+                    height: 40px;
+                    width: 40px;
+                    .item-3-title{
+                        font-size:16px;
+                        font-family:PingFangSC-Regular,PingFangSC;
+                        font-weight:400;
+                        color:rgba(3,8,24,1);
+                    }
+                    .item-3-title2{
+                        margin-top: 2px;
+                        font-size:12px;
+                        font-family:PingFangSC-Regular,PingFangSC;
+                        font-weight:400;
+                        color:rgba(138,143,155,1);
+                    }
+                }
+                .item-3:last-of-type{
+                    margin-right: 0;
+                }
+            }
+        }
+    }
+}
+</style>
--- src/components/appointment/index.vue
+++ src/components/appointment/index.vue
@@ -1,18 +1,50 @@
 <template>
     <div class="container">
+        <resume></resume>
+        <div class="interview-container col flex-item-start flex-justify-start">
+            <div class="title">
+                请选择面试方式
+            </div>
+            <div class="title2 row flex-item flex-justify-start">
+                  <div class="item item-active">
+                      到店面试
+                  </div>
+                <div class="item item-normal">
+                    上门面试
+                </div>
+                <div class="item item-normal">
+                    视频面试
+                </div>
+            </div>
+            <div class="title3 row flex-item flex-justify-start">
+                <div class="text1">到店面试地址：</div>
+                <div class="text2">北京市朝阳区金茂府2期东区6号楼</div>
+            </div>
+
+        </div>
+        <div class="appointment-btn row flex-item flex-justify-between">
+          <div class="title1">预约面试时间</div>
+          <div class="title2 row flex-item flex-justify-between">
+              <div class="item1">请选择</div>
+              <div class="item2">></div>
+          </div>
+        </div>
     </div>
 </template>
 
 <script>
+  import resume from '@/common/components/resume/index';
   export default {
-    name: "index",
+    name: "apponitment",
     data() {
       return {
 
       }
     },
 //组件
-    components: {},
+    components: {
+      resume
+    },
 //初始化数据
     created() {
 
@@ -29,6 +61,97 @@
         width: 100%;
         height: 100%;
         overflow: hidden;
-        background:$white;
+        background:#f7f7f7;
+    }
+    .interview-container{
+        margin-top: 10px;
+        background: $white;
+        width: 100%;
+        padding-left: 16px;
+        min-height: 140px;
+        .title{
+            margin-top: 16px;
+            width: 100%;
+            height: 20px;
+            line-height: 20px;
+            font-size:16px;
+            font-family:PingFangSC-Medium,PingFangSC;
+            font-weight:600;
+            color:#000212;
+        }
+        .title2{
+            margin-top: 16px;
+            width: 100%;
+            height: 30px;
+            .item{
+                width: 102px;
+                height: 28px;
+                line-height: 28px;
+                border-radius:6px;
+                font-size:13px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                text-align: center;
+                margin-right: 19px;
+                box-sizing: border-box;
+            }
+            .item-normal{
+                color:rgba(0,2,18,1);
+                background:rgba(241,241,241,1);
+            }
+            .item-active{
+                border:1px solid;
+                border-image:linear-gradient(338deg, rgba(201,141,253,1), rgba(250,119,119,1)) 1 1;
+                color:rgba(0,2,18,1);
+                background:linear-gradient(338deg, rgba(201,141,253,1) 0%, rgba(250,119,119,1) 100%);
+                -webkit-background-clip:text;
+                -webkit-text-fill-color:transparent;
+            }
+
+        }
+        .title3{
+            margin-top: 16px;
+            height: 20px;
+            line-height: 20px;
+            text-align: left;
+            font-size:13px;
+            font-family:PingFangSC-Regular,PingFangSC;
+            font-weight:400;
+            .text1{
+                color:rgba(138,143,155,1);
+            }
+            .text2{
+                color:#3B445C;
+            }
+        }
+    }
+    .appointment-btn{
+        margin-top: 10px;
+        height: 52px;
+        line-height: 52px;
+        width: 100%;
+        background: #FFFFFF;
+        .title1{
+            margin-left: 16px;
+            font-size:16px;
+            font-family:PingFangSC-Medium,PingFangSC;
+            font-weight:600;
+            color:rgba(0,2,18,1);
+        }
+        .title2{
+            height: 100%;
+            margin-right: 16px;
+            .item1{
+                font-size:13px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                color:rgba(200,201,203,1);
+                margin-right: 10px;
+            }
+            .item2{
+                color: #949EA4;
+            }
+
+        }
     }
 </style>
