fac03514cb2b5eb425470d14aa3a3f26478c71d5
xionghq <18614084016@163.com>
2019-10-11 11:26:41 +0800

    订单详情页面

--- src/components/orderDetail/index.vue
+++ src/components/orderDetail/index.vue
@@ -1,17 +1,65 @@
 <template>
     <div class="container">
-        this is order-detail-page
+        <resume></resume>
+        <div class="order-item">
+            <div class="item1 row flex-item flex-justify-between">
+                <div class="row flex-item flex-justify-start">
+                    <div class="item-icon">
+
+                    </div>
+                    <div class="occupation">
+                        面试信息
+                    </div>
+                </div>
+                <!--          <div class="common-status item-status">-->
+                <!--             待面试-->
+                <!--          </div>-->
+                <!--          <div class="common-status item-status1">-->
+                <!--            面试结束-->
+                <!--          </div>-->
+                <div class="common-status item-status2">
+                    待签约
+                </div>
+            </div>
+            <div class="item3 row flex-item flex-justify-start">
+                <div class="item-title">
+                    面试方式：
+                </div>
+                <div class="content">
+                    上门面试
+                </div>
+            </div>
+            <div class="item3 row flex-item flex-justify-start">
+                <div class="item-title">
+                    面试时间：
+                </div>
+                <div class="content">
+                    2019-09-09
+                </div>
+            </div>
+            <div class="item4 row flex-item flex-justify-start">
+                <div class="item-title">
+                    面试地点：
+                </div>
+                <div class="content">
+                    广东省广州市天河区XXXXXX
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    import resume from '@/common/components/resume/index';
     export default {
-        name: "index",
+        name: "orderDetail",
         data() {
             return {}
         },
 //组件
-        components: {},
+        components: {
+          resume
+        },
 //初始化数据
         created() {
 
@@ -26,49 +74,78 @@
         width: 100%;
         overflow: hidden;
     }
-    .header{
-        width: 750px;
-        height: 750px;
-        background: #FFFFFF;
-        .img-view{
-            width: 366px;
-            height: 507px;
-            background: #F7F6FB;
-        }
-    }
-    .commodity-info{
-        min-height: 220px;
+    .order-item{
+        background: $white;
         width: 100%;
-        background: #ffffff;
-        margin-top: 20px;
-        .money{
-            height: 100px;
-            line-height: 100px;
-            width: 678px;
-            text-align: left;
-            color: #ff6666;
-            font-size: 40px;
-        }
-        .content{
-            color: #3A3E51FF;
-            width: 678px;
-            min-height: 80px;
-            line-height: 44px;
-            font-size: 32px;
-            margin-bottom: 32px;
-        }
-    }
-    .address{
-        margin-top: 20px;
-        width: 100%;
-        height: 240px;
-        background: #FFFFFF;
         overflow: hidden;
-        .item{
-            margin-top: 30px;
-            height: 40px;
-            width: 678px;
-            background: #F7F6FB;
+        margin-bottom: 10px;
+        .item1{
+            width: 100%;
+            height: 30px;
+            margin-bottom: 8px;
+            .item-icon{
+                margin-left: 16px;
+                margin-right: 10px;
+                background: #666666;
+                width: 14px;
+                height: 16px;
+            }
+            .occupation{
+                font-size:16px;
+                font-family:PingFangSC-Medium,PingFangSC;
+                font-weight:500;
+                color:rgba(0,2,18,1);
+            }
+            .common-status{
+                margin-right: 16px;
+                font-size:13px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+            }
+            .item-status{
+                color:#E9AC7E;
+            }
+            .item-status1{
+                color:#CECFD1;
+            }
+            .item-status2{
+                color:#7DB5FB;
+            }
+        }
+        .item3{
+            width: 100%;
+            height: 24px;
+            .item-title{
+                margin-left: 16px;
+                font-size:13px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                color:rgba(138,143,155,1);
+            }
+            .content{
+                font-size:13px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                color:#3B445C;
+            }
+        }
+        .item4{
+            width: 100%;
+            height: 24px;
+            margin-bottom: 20px;
+            .item-title{
+                margin-left: 16px;
+                font-size:13px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                color:rgba(138,143,155,1);
+            }
+            .content{
+                font-size:13px;
+                font-family:PingFangSC-Regular,PingFangSC;
+                font-weight:400;
+                color:#3B445C;
+            }
         }
     }
 </style>
