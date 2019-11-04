2723d116fb883ff3207231c7c05c9b9fa7a0c56e
xionghq <18614084016@163.com>
2019-11-04 10:57:28 +0800

    提交代码

--- src/components/myCollect/index.vue
+++ src/components/myCollect/index.vue
@@ -1,66 +1,48 @@
 <template>
-  <div class="container">
-    <van-swipe-cell class="item-container">
-<!--      <template slot="left">-->
-<!--        <van-button square type="primary" text="选择" />-->
-<!--      </template>-->
-
-<!--      <van-cell :border="false" title="单元格" value="内容" />-->
-        <div class="content">
-          123
-        </div>
-      <span slot="right">test</span>
-<!--      <template slot="right">-->
-<!--       <div class="delete">-->
-<!--          删除-->
-<!--       </div>-->
-<!--      </template>-->
-    </van-swipe-cell>
-  </div>
+    <div class="container">
+        <li v-for="(item,index) of list" :key="index">
+            <van-swipe-cell :right-width="70" :left-width="0">
+                <van-cell-group>
+                    <staffInfo></staffInfo>
+                </van-cell-group>
+                <div class="delete" slot="right">删除</div>
+            </van-swipe-cell>
+        </li>
+    </div>
 </template>
 <script>
-    export default {
-        name: "myCollect",
-        data() {
-            return {
-            }
-        },
-        components:{
-        },
-        mounted() {
+  import staffInfo from '@/common/components/staffInfo'
 
-        },
-        methods: {
+  export default {
+    name: 'myCollect',
+    data () {
+      return {
+        list: [1, 2, 3]
+      }
+    },
+    components: {
+      staffInfo
+    },
+    mounted () {
 
-        },
-        created(){
+    },
+    methods: {},
+    created () {
 
-        }
     }
+  }
 </script>
 <style scoped lang="scss">
-  .container{
-    width: 100%;
-    height: 100%;
-    background: #efefef;
-    overflow: hidden;
-  }
-
-  .item-container{
-    background: #7DB5FB;
-    height: 112px;
-    width: 375px;
-  }
-  .content{
-    width: 375px;
-    height: 112px;
-    text-align: center;
-    background: red;
-  }
-  .delete{
-    width: 70px;
-    height: 76px;
-    background:rebeccapurple;
-    text-align: center;
-  }
+    .delete {
+        margin-top: 18px;
+        height: 76px;
+        width: 70px;
+        line-height: 76px;
+        text-align: center;
+        background: #f44;
+        font-size: 14px;
+        font-family: PingFangSC-Regular, PingFang SC;
+        font-weight: 400;
+        color: rgba(255, 255, 255, 1);
+    }
 </style>
