f17d93560d2e9aa373f5453e9214875ff28a4a4f
xhq <651619994@qq.com>
2019-11-05 17:08:42 +0800

    提交代码

--- src/common/vant/index.js
+++ src/common/vant/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import 'vant/lib/index.css';
-import {Swipe,SwipeItem,Toast,Field,Cell,CellGroup,Popup,DatetimePicker,List,SwipeCell,Button,Rate,Dialog} from 'vant';
+import {Swipe,SwipeItem,Toast,Field,Cell,CellGroup,Popup,DatetimePicker,List,SwipeCell,Button,Rate,Dialog,Loading} from 'vant';
 import VueKeepScrollPosition from 'vue-keep-scroll-position';
 Vue.use(VueKeepScrollPosition);
 Vue.use(Swipe).use(SwipeItem);
@@ -13,6 +13,7 @@ Vue.use(SwipeCell); //滑动单元格
 Vue.use(Button);
 Vue.use(Rate);
 Vue.use(Dialog);
+Vue.use(Loading);
 Vue.prototype.$toast = Toast;
 Vue.prototype.$loading = (param)=>{
   if(!param){
--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="container">
-    <van-list
-      v-model="load.loading"
-      :finished="load.finished"
-      finished-text="没有更多了"
-      :error.sync="load.error"
-      error-text="请求失败，点击重新加载"
-      @load="loadEvent"
-    >
+<!--    <van-list-->
+<!--      v-model="load.loading"-->
+<!--      :finished="load.finished"-->
+<!--      finished-text="没有更多了"-->
+<!--      :error.sync="load.error"-->
+<!--      error-text="请求失败，点击重新加载"-->
+<!--      @load="loadEvent"-->
+<!--    >-->
+
+
       <div class="header-box">
         <swiper :options="swiperOption" class="swipe-container">
           <swiper-slide v-for="(slide, index) in swiperSlides" :key="index"
@@ -27,20 +29,25 @@
       <swiper :options="switchOptions" ref="mySwiper" class="switch-container"
               @slideChangeTransitionEnd="slideChangeTransitionEndCallback">
         <swiper-slide class="switch-item">
-          <staffInfo v-for="(item ,index) in list1" :key="index" :itemInfo="item"></staffInfo>
+          <staffInfo v-for="(item ,index) in list1" :key="index" :itemInfo="item" v-if="list1.length>0"></staffInfo>
+          <div style="width: 100%;text-align: center;color: #9c9c9c;margin-top: 80px" v-if="list1.length==0">没有更多数据了</div>
         </swiper-slide>
         <swiper-slide class="switch-item">
-          <staffInfo v-for="(item ,index) in list2" :key="index" :itemInfo="item"></staffInfo>
+          <staffInfo v-for="(item ,index) in list2" :key="index" :itemInfo="item"  v-if="list2.length>0"></staffInfo>
+          <div style="width: 100%;text-align: center;color: #9c9c9c;margin-top: 80px" v-if="list2.length==0">没有更多数据了</div>
         </swiper-slide>
         <swiper-slide class="switch-item">
-          <staffInfo v-for="(item ,index) in list3" :key="index" :itemInfo="item"></staffInfo>
+          <staffInfo v-for="(item ,index) in list3" :key="index" :itemInfo="item" v-if="list3.length>0"></staffInfo>
+          <div style="width: 100%;text-align: center;color: #9c9c9c;margin-top: 80px" v-if="list3.length==0">没有更多数据了</div>
         </swiper-slide>
         <swiper-slide class="switch-item">
-          <staffInfo2 v-for="(item ,index) in list4" :key="index"></staffInfo2>
+          <staffInfo2 v-for="(item ,index) in list4" :key="index" :itemInfo="item" v-if="list4.length>0"></staffInfo2>
+          <div style="width: 100%;text-align: center;color: #9c9c9c;margin-top: 80px" v-if="list4.length==0">没有更多数据了</div>
         </swiper-slide>
       </swiper>
-    </van-list>
+<!--    </van-list>-->
     <backTop :backTop="backTop"></backTop>
+    <div class="load row flex-item flex-justify" v-if="loading"><van-loading size="24px"></van-loading><div class="text">加载中...</div></div>
   </div>
 </template>
 <script>
@@ -102,17 +109,14 @@
                 list2: [], //育儿嫂列表
                 list3: [], //保姆列表
                 list4: [], //更多列表
-                load: {
-                    error: false,  //错误
-                    loading: false, //加载更多
-                    finished: false //完成
-                },
                 page: {
                     currentMoonWomanPage: 1,  //月嫂
                     currentChildRearingPage: 1, //育儿嫂
                     currentBabySitterPage: 1, //保姆
                     currentMorePage: 1        //更多
-                }
+                },
+                isLocked:false, //滑动加锁
+                loading:true
 
             }
         },
@@ -135,7 +139,8 @@
 
         },
         mounted() {
-            window.addEventListener('scroll', this.watchScroll)
+            window.addEventListener('scroll', this.watchScroll);
+
             console.log(this.screenWidth)
             const that = this
             window.onresize = () => {
@@ -146,6 +151,7 @@
                 })()
             };
             this.updateBannerList();
+            this.updateList();
         },
         destroyed() {
             window.removeEventListener('scroll', this.watchScroll)
@@ -159,10 +165,10 @@
                     let [err,data]=await getEmployeeList({type:1,pageNum:this.page.currentMoonWomanPage});
                     if(err!==null){this.$toast(err||'系统错误');this.$clear();return ;};
                     let list=data.list||[];
-                    if(list.length==0){this.$toast('没有更多了');this.$clear();return ;};
+                    if(list.length==0){this.$clear();return ;};
                     this.list1=[...this.list1,...list];
                     this.page.currentMoonWomanPage+=1;
-                    this.load.loading=false;
+                    // this.load.loading=false;
                     this.$clear();
                     return ;
                 };
@@ -170,10 +176,10 @@
                     let [err2,data2]=await getEmployeeList({type:2,pageNum:this.page.currentChildRearingPage});
                     if(err2!==null){this.$toast(err2||'系统错误');this.$clear();return ;};
                     let list=data2.list||[];
-                    if(list.length==0){this.$toast('没有更多了');this.$clear();return ;};
+                    if(list.length==0){this.$clear();return ;};
                     this.list2=[...this.list2,...list];
                     this.page.currentChildRearingPage+=1;
-                    this.load.loading=false;
+                    // this.load.loading=false;
                     this.$clear();
                     return ;
                 }
@@ -181,10 +187,10 @@
                     let [err3,data3]=await getEmployeeList({type:3,pageNum:this.page.currentBabySitterPage});
                     if(err3!==null){this.$toast(err3||'系统错误');this.$clear();return ;};
                     let list=data3.list||[];
-                    if(list.length==0){this.$toast('没有更多了');this.$clear();return ;};
+                    if(list.length==0){this.$clear();return ;};
                     this.list3=[...this.list3,...list];
                     this.page.currentBabySitterPage+=1;
-                    this.load.loading=false;
+                    // this.load.loading=false;
                     this.$clear();
                     return ;
                 }
@@ -192,10 +198,10 @@
                     let [err4,data4]=await getEmployeeList({type:4,pageNum:this.page.currentMorePage});
                     if(err4!==null){this.$toast(err4||'系统错误');this.$clear();return ;};
                     let list=data4.list||[];
-                    if(list.length==0){this.$toast('没有更多了');this.$clear();return ;};
+                    if(list.length==0){this.$clear();return ;};
                     this.list4=[...this.list4,...list];
                     this.page.currentMorePage+=1;
-                    this.load.loading=false;
+                    // this.load.loading=false;
                     this.$clear();
                     return ;
                 }
@@ -257,11 +263,11 @@
                 this.nowIndex = this.swicthSwiper.activeIndex
                 this.updateList();
             },
-            watchScroll() {
-                // let clientHeight  = document.documentElement.clientHeight; //浏览器高度
-                // let scrollHeight = document.body.scrollHeight; //滚动高度
-                let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop //滚动视窗的高度距离window顶部的距离，它会随着往上滚动而不断增加，初始值是0，它是一个变化的值；
-                // let distance = 50;  //距离视窗还用50的时候，开始触发；
+            async watchScroll() {
+                let clientHeight  = document.documentElement.clientHeight; //浏览器高度
+                let scrollHeight = document.body.scrollHeight; //滚动高度
+                let distance = -50;  //距离视窗还用50的时候，开始触发；
+                let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop; //滚动视窗的高度距离window顶部的距离，它会随着往上滚动而不断增加，初始值是0，它是一个变化的值；
                 //  当滚动超过 50 时，实现吸顶效果
                 if (scrollTop > 206 * this.screenWidth / 375) {
                     this.navBarFixed = true
@@ -274,10 +280,20 @@
                     this.backTop=false;
                 }
 
+
                 //是否触底
-                // if ((scrollTop + clientHeight) >= (scrollHeight - distance)) {
-                //   console.log("到底了，开始加载数据");
-                // };
+                if ((scrollTop + clientHeight) >= (scrollHeight - distance)) {
+                    if(this.isLocked){
+                        console.log('加锁了');
+                        return ;
+                    }
+                    this.loading=true;
+                    console.log("到底了，开始加载数据");
+                    this.isLocked=true;
+                    await  this.loadEvent();
+                    this.isLocked=false;
+                    this.loading=false;
+                };
             },
             //更新头部bannerList 数据
             async updateBannerList() {
@@ -369,7 +385,16 @@
     width: 100%;
     .switch-item {
       width: 100%;
+      min-height: 350px;
     }
 
+  }
+  .load{
+    .text{
+      margin-left: 10px;
+      color: #c9c9c9;
+      font-size: 14px;
+      font-family: PingFangSC-Regular, PingFangSC;
+    }
   }
 </style>
