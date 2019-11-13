9c63f08fc3c0ef3c6116c4e6201467295e2eb53d
xhq <651619994@qq.com>
2019-11-13 10:53:35 +0800

    加了 better-scroll 版本

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -1,14 +1,6 @@
 <template>
-  <div class="container">
-<!--    <van-list-->
-<!--      v-model="load.loading"-->
-<!--      :finished="load.finished"-->
-<!--      finished-text="没有更多了"-->
-<!--      :error.sync="load.error"-->
-<!--      error-text="请求失败，点击重新加载"-->
-<!--      @load="loadEvent"-->
-<!--    >-->
-
+  <div class="container" ref="wrapper" :scrollbar="options.scrollbar" :startY="options.startY">
+    <div class="content">
       <div class="header-box">
         <swiper :options="swiperOption" class="swipe-container">
           <swiper-slide v-for="(slide, index) in swiperSlides" :key="index"
@@ -49,9 +41,11 @@
     <Service :is-show="serviceData.isShow" @cancelEvent="serviceCancelEvent" @sureEvent="serviceSureEvent" @closeEvent="serviceCloseEvent"></Service>
     <div class="load row flex-item flex-justify" v-if="loading&&((nowIndex==0&&(list1.length>0))||(nowIndex==1&&(list2.length>0))||(nowIndex==2&&(list3.length>0))||(nowIndex==3&&(list4.length>0)))"><van-loading size="24px"></van-loading><div class="text">加载中...</div></div>
     <div class="load row flex-item flex-justify" v-if="finish&&!loading&&((nowIndex==0&&(list1.length>0))||(nowIndex==1&&(list2.length>0))||(nowIndex==2&&(list3.length>0))||(nowIndex==3&&(list4.length>0)))"><div class="text">没有更多数据了</div></div>
+    </div>
   </div>
 </template>
 <script>
+    import BScroll from 'better-scroll';
     import 'swiper/dist/css/swiper.css'
     import {swiper, swiperSlide} from 'vue-awesome-swiper'
     import staffInfo from '@/common/components/staffInfo'
@@ -130,8 +124,23 @@
                     currentBabySitterPage: 1, //保姆
                     currentMorePage: 1        //更多
                 },
+                options: {
+                    pullDownRefresh: {
+                        threshold: 50, // 当下拉到超过顶部 50px 时，触发 pullingDown 事件
+                        stop: 20 // 刷新数据的过程中，回弹停留在距离顶部还有 20px 的位置
+                    },
+                    pullUpLoad: {
+                        threshold: -20 // 在上拉到超过底部 20px 时，触发 pullingUp 事件
+                    },
+//     pullDownRefresh: false, //关闭下拉
+//     pullUpLoad: false, // 关闭上拉
+                    click: true,
+                    probeType: 3,
+                    startY: 0,//欢迎加入全栈开发交流圈一起学习交流：864305860
+                    scrollbar: true//面向1-3年前端人员
+                },//帮助突破技术瓶颈，提升思维能力
                 isLocked:false, //滑动加锁
-                loading:true,
+                loading:false,
                 finish:false
 
             }
@@ -156,10 +165,6 @@
 
         },
         mounted() {
-            window.addEventListener('scroll', this.watchScroll);
-            window.addEventListener('scroll', this.watchScroll2);
-
-            console.log(this.screenWidth)
             const that = this
             window.onresize = () => {
                 return (() => {
@@ -170,16 +175,13 @@
             };
             this.updateBannerList();
             this.updateList();
+            this.load();
         },
         destroyed() {
-            window.removeEventListener('scroll', this.watchScroll);
-            window.removeEventListener('scroll', this.watchScroll2);
         },
 //一些自定义方法
         methods: {
             async loadEvent() {
-                console.log('loadEvent is run');
-
                 if(this.nowIndex==0){
                     this.$loading();
                     let [err,data]=await getEmployeeList({type:1,pageNum:this.page.currentMoonWomanPage});
@@ -188,7 +190,6 @@
                     if(this.page.currentMoonWomanPage-1>=totalPage){this.$clear();this.finish=true;return ;};
                     this.list1=[...this.list1,...list];
                     this.page.currentMoonWomanPage+=1;
-                    // this.load.loading=false;
                     this.$clear();
                     return ;
                 };
@@ -200,7 +201,6 @@
                     if(this.page.currentChildRearingPage-1>=totalPage){this.$clear();this.finish=true;return ;};
                     this.list2=[...this.list2,...list];
                     this.page.currentChildRearingPage+=1;
-                    // this.load.loading=false;
                     this.$clear();
                     return ;
                 }
@@ -212,19 +212,10 @@
                     if(this.page.currentBabySitterPage-1>=totalPage){this.$clear();this.finish=true;return ;};
                     this.list3=[...this.list3,...list];
                     this.page.currentBabySitterPage+=1;
-                    // this.load.loading=false;
                     this.$clear();
                     return ;
                 }
                 if(this.nowIndex==3){
-                    // let [err4,data4]=await getEmployeeList({type:4,pageNum:this.page.currentMorePage});
-                    // if(err4!==null){this.$toast(err4||'系统错误');this.$clear();return ;};
-                    // let list=data4.list||[];
-                    // if(list.length==0){this.$clear();this.finish=true;return ;};
-                    // this.list4=[...this.list4,...list];
-                    // this.page.currentMorePage+=1;
-                    // // this.load.loading=false;
-                    // this.$clear();
                     return ;
                 }
             },
@@ -264,14 +255,6 @@
                     return ;
                 }
                 if(this.nowIndex==3){
-                    // if(this.list4.length>0){return;};
-                    // this.$loading();
-                    // let [err4,data4]=await getEmployeeList({type:4,pageNum:1});
-                    // if(err4!==null){this.$toast(err4||'系统错误');this.$clear();return ;};
-                    // let list=data4.list||[];
-                    // this.list4=[...list];
-                    // this.page.currentMorePage=1;
-                    // this.$clear();
                     return ;
                 }
 
@@ -285,10 +268,8 @@
                 this.nowIndex = this.swicthSwiper.activeIndex
                 this.updateList();
             },
-            async watchScroll() {
-                // let clientHeight  = document.documentElement.clientHeight; //浏览器高度
-                // let scrollHeight = document.body.scrollHeight; //滚动高度
-                // let distance = -30;  //距离视窗还用50的时候，开始触发；
+            watchScroll() {
+                console.log('watchScroll==>',new Date().getTime());
                 let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop; //滚动视窗的高度距离window顶部的距离，它会随着往上滚动而不断增加，初始值是0，它是一个变化的值；
                 //  当滚动超过 50 时，实现吸顶效果
                 if (scrollTop > 206 * this.screenWidth / 375) {
@@ -301,26 +282,12 @@
                 }else{
                     this.backTop=false;
                 }
-
-
-                //是否触底
-                // if ((scrollTop + clientHeight) >= (scrollHeight - distance)) {
-                //     if(this.isLocked){
-                //         console.log('加锁了');
-                //         return ;
-                //     }
-                //     this.loading=true;
-                //     console.log("到底了，开始加载数据");
-                //     this.isLocked=true;
-                //     await  this.loadEvent();
-                //     this.isLocked=false;
-                //     this.loading=false;
-                // };
+                // this.navBarFixed = true
             },
             watchScroll2:Throttle(async function(){
                 let clientHeight  = document.documentElement.clientHeight; //浏览器高度
                 let scrollHeight = document.body.scrollHeight; //滚动高度
-                let distance = -30;  //距离视窗还用50的时候，开始触发；
+                let distance = 50;  //距离视窗还用50的时候，开始触发；
                 let scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop; //滚动视窗的高度距离window顶部的距离，它会随着往上滚动而不断增加，初始值是0，它是一个变化的值；
                 //是否触底
                 if ((scrollTop + clientHeight) >= (scrollHeight - distance)) {
@@ -332,6 +299,7 @@
                     console.log("到底了，开始加载数据");
                     this.isLocked=true;
                     await  this.loadEvent();
+                    console.log('loadEvent is finish')
                     this.isLocked=false;
                     this.loading=false;
                 };
@@ -358,9 +326,40 @@
             },
             handleShowService(){
                 this.serviceData.isShow=true;
-            }
+            },
+            load(){
+                if (!this.scroll) {
+                    this.scroll = new BScroll(this.$refs.wrapper, this.options);
+                    // 上拉
+                    this.scroll.on('pullingUp', () => {
+                        // 刷新数据的过程中，回弹停留在距离顶部还有20px的位置
+                        this.setData();
+                    });
+                    //实时监听
+                    this.scroll.on('scroll',()=>{
 
+                        this.watchScroll();
 
+                    });
+                } else {//欢迎加入全栈开发交流圈一起学习交流：864305860
+                    this.scroll.refresh()//面向1-3年前端人员
+                }//帮助突破技术瓶颈，提升思维能力
+            },
+            async setData() {
+                this.loading=true;
+                console.log("到底了，开始加载数据");
+                // this.isLocked=true;
+                await  this.loadEvent();
+                console.log('loadEvent is finish')
+                // this.isLocked=false;
+                this.loading=false;
+
+                this.scroll.finishPullUp();
+                this.pullingDownUp()
+            },
+            pullingDownUp() {
+                    this.scroll.refresh() //重新计算元素高度
+            },
         }
     }
 </script>
@@ -369,8 +368,11 @@
   .container {
     width: 100%;
     background: #F7F6FB;
-    padding-bottom: 80px;
+    box-sizing: border-box;
     overflow: hidden;
+    position: absolute;
+    top: 0;
+    bottom: 50px;
   }
 
   .header-box {
