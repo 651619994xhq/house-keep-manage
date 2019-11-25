a66bfdc8636a2046082aa3eb5a2438a3b44fe54d
xhq <651619994@qq.com>
2019-11-25 21:58:56 +0800

    提交代码

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -14,7 +14,7 @@ export default {
     state.token = '';
     const h5BaseURL = process.env.H5_BASE_URL;
     // window.location.reload();
-    window.location=`${h5BaseURL}/#/enterport?time=${new Date().getTime()}`
+    window.location.href=`${h5BaseURL}/#/enterport?time=${new Date().getTime()}`;
   },
 
 }
